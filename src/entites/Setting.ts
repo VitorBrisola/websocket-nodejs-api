52300ba7bb1266ab14944d3674695e71522a2421
Vitor Brisola <vitoraugusto199@gmail.com>
2021-04-25 17:13:15 -0300

    add entites

---
+++ src/entites/Setting.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, UpdateDateColumn, CreateDateColumn, PrimaryColumn } from 'typeorm';
+import {v4 as uuid} from 'uuid';
+
+@Entity("settings")
+class Setting {
+
+    @PrimaryColumn()
+    id: string;
+    
+    @Column({ name: "user_name" })
+    userName: string; 
+
+    @Column()
+    chat: boolean;
+
+    @UpdateDateColumn({name: 'updated_at'})
+    updatedAt: Date;
+
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: Date;
+
+    constructor() {
+        if(!this.id){
+            this.id = uuid();
+        }
+    }
+}
+
+export default Setting;
